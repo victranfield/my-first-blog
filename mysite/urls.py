5c67e1cb73a0ffa5722f268a0d56ed6211f598a3
Catherine Tranfield <cat.tranfield@gmail.com>
2019-06-14 17:10:55 +0100

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+"""mysite URL Configuration
+
+[...]
+"""
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
+
